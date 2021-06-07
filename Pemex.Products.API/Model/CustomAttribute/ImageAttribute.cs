3a04d3a4576da496c4a3b0e651084d5210ac8f0d
Lvcios <lfloresg0801@gmail.com>
2021-06-06 23:01:57 -0500

    fix mime type svg

--- Pemex.Products.API/Model/CustomAttribute/ImageAttribute.cs
+++ Pemex.Products.API/Model/CustomAttribute/ImageAttribute.cs
@@ -9,7 +9,7 @@ namespace Pemex.Products.API.Model.CustomAttribute
 {
     public class ImageAttribute: ValidationAttribute
     {
-        public string[] ValidImageMimeTypes = { "image/gif", "image/jpg", "image/jpeg", "image/image/svg+xml", "image/tiff", "image/bmp", "image/png" };
+        public string[] ValidImageMimeTypes = { "image/gif", "image/jpg", "image/jpeg", "image/svg+xml", "image/tiff", "image/bmp", "image/png" };
         public ImageAttribute()
         {
             
